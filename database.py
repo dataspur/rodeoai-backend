f3d2bc0060957aa5b3942648e007295e976664d7
dataspur <trevor@rodeoai.rodeo>
2025-10-17 23:35:17 -0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_db():
+    async with async_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
